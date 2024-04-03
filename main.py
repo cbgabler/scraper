3de70b3e2321f314677f0279a7bbbc91dba94fde
Carson Gabler <carsongabler7@gmail.com>
2024-04-03 12:00:02 -0700

    feat: linked main and web_parse

--- main.py
+++ main.py
@@ -3,23 +3,13 @@ import web_parse as wp
 import config
 import constants as cts
 import webbrowser as wb
-import time
-import headers
 from bs4 import BeautifulSoup
 
 def main():
     driver = wp.start_driver()
-    driver.get("https://www.basketball-reference.com/players/t/tatumja01.html")
-
-    time.sleep(5)
-
-    html_content = driver.page_source
-    soup = BeautifulSoup(html_content, "html.parser")
-
-    print(soup)
-
-    # Close the WebDriver session
-    driver.quit()
+    html = wp.extract_points("https://www.basketball-reference.com/players/t/tatumja01.html", driver)
+    print(html)
+    return 0
 
 if __name__ == '__main__':
     main()
--- web_parse.py
+++ web_parse.py
@@ -5,16 +5,16 @@ import undetected_chromedriver as uc
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
-from fake_headers import Headers
 import csv
-import headers
 from seleniumwire import webdriver
 import undetected_chromedriver as uc
 from user_agents import user_agents
 import random
+from cleanup import cleanup
 
 def start_driver():
     random_user_agent = random.choice(user_agents)
+    print(random_user_agent)
     options = uc.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument(f"user-agent={random_user_agent}")
@@ -22,9 +22,10 @@ def start_driver():
     return driver
 
 def extract_points(player_url, driver):
-    header = headers.generate_headers()
     driver.get(player_url)
     print(driver.page_source)
     player_html = BeautifulSoup(driver.page_source, 'html.parser')
-    ml_prop = player_html.findAll("a", class_="bet-link" )
-    return ml_prop
+    driver.quit()
+    player = player_html.title.string
+    clean_html = cleanup(player_url, player)
+    return clean_html
