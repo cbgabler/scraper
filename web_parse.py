f080d7fbd30c09ca4d4957c36e5ed137308947ef
Carson Gabler <carsongabler7@gmail.com>
2024-04-16 14:50:24 -0700

    feat: web_parse w/o SQL functionality

--- web_parse.py
+++ web_parse.py
@@ -1,15 +1,11 @@
-import sqlite3
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
 import undetected_chromedriver as uc
 from user_agents import user_agents
 import random
 import time
+import csv
 import constants as cts
-
-# Connect to your SQLite database
-conn = sqlite3.connect('your_database.db')
-cursor = conn.cursor()
 
 def instantiate_driver():
     ## collect random user agent to mask browser
@@ -24,13 +20,13 @@ def instantiate_driver():
 
 def extract_data(bookie_urls, driver):
     for bookie, url in bookie_urls.items():
-        print(f"Now extracting {bookie}")
+        print(f"Now extracting{bookie}")
         driver.get(url)
         html = driver.page_source
         time.sleep(2)
         soup = BeautifulSoup(html, "lxml")
         divs = soup.find_all("div", class_=cts.MAIN_DIV[bookie])
-        
+
         today_games = []
         tomorrow_games = []
         for div in divs:
@@ -58,16 +54,17 @@ def extract_data(bookie_urls, driver):
             for team, ml in zip(mlb_team, mlb_ml):
                 tomorrow_data[team.text.strip()] = ml.text.strip()
 
-        # Insert data into the tables
-        cursor.execute('CREATE TABLE IF NOT EXISTS today_games (Team TEXT, Moneyline TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS tomorrow_games (Team TEXT, Moneyline TEXT)')
+        with open(f"{bookie}_today_games.csv", mode='w', newline='') as today_file:
+            fieldnames = ["Team", "Moneyline"]
+            writer = csv.DictWriter(today_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for team, moneyline in today_data.items():
+                writer.writerow({"Team": team, "Moneyline": moneyline})
 
-        for team, moneyline in today_data.items():
-            cursor.execute('INSERT INTO today_games (Team, Moneyline) VALUES (?, ?)', (team, moneyline))
+        with open(f"{bookie}_tomorrow_games.csv", mode='w', newline='') as tomorrow_file:
+            fieldnames = ["Team", "Moneyline"]
+            writer = csv.DictWriter(tomorrow_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for team, moneyline in tomorrow_data.items():
+                writer.writerow({"Team": team, "Moneyline": moneyline})
 
-        for team, moneyline in tomorrow_data.items():
-            cursor.execute('INSERT INTO tomorrow_games (Team, Moneyline) VALUES (?, ?)', (team, moneyline))
-
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
