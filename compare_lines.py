c2eac2d70d784219d3d1a8dff1de8bed40ccfd79
Carson Gabler <carsongabler7@gmail.com>
2024-05-08 21:05:59 -0700

    up to date, TODO fix html csv reading

--- compare_lines.py
+++ compare_lines.py
@@ -1,5 +1,8 @@
+from flask import Flask, render_template
 import os
 import csv
+
+app = Flask(__name__)
 
 # Path to the CSV file
 csv_file_path = os.path.join(os.path.dirname(__file__), 'sport_csvs', 'dk_baseball_tomorrow.csv')
@@ -8,5 +11,14 @@ csv_file_path = os.path.join(os.path.dirname(__file__), 'sport_csvs', 'dk_baseba
 def read_csv():
     with open(csv_file_path, mode='r') as file:
         csv_reader = csv.reader(file)
-        for row in csv_reader:
-            print(row)
+        data = [row for row in csv_reader]
+    return data
+
+# Endpoint to display the CSV data in your HTML
+@app.route('/')
+def index():
+    data = read_csv()
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
