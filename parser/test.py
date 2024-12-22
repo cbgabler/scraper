17e25ba51e1ee530db3fbe8e990be42da6083e08
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2024-12-21 17:11:30 -0800

    Added: Multi-URL parsing // Sub-URL needed

--- parser/test.py
+++ parser/test.py
@@ -6,20 +6,22 @@ import web_parse as wp
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 
 def test_clicking(driver, db):
-    action = ActionChains(driver)
-    driver.get("https://sportsbook.draftkings.com/")
-    popular = driver.find_element(By.CLASS_NAME, "sportsbook-expandable-shell__wrapper")
-    popular_clickables = popular.find_elements(By.CLASS_NAME, "sportsbook-navitation-item-title-text")
-    for clickable in popular_clickables:
-        clickable.click()
-        driver.get(driver.current_url)
+    i = 1
+    while i <= 15:
+        action = ActionChains(driver)
+        driver.get("https://sportsbook.draftkings.com/")
+        driver.find_element(By.XPATH, f"//*[@id='root']/section/section[2]/div[1]/div[1]/div[2]/ul/li[{i}]/a").click()
+        print(driver.current_url)
+        ## popular_clickables = popular.find_elements(By.CLASS_NAME, "sportsbook-navitation-item-title-text")
+        ## popular_clickables[i].click()
         sub_clickables = driver.find_elements(By.CLASS_NAME, "sportsbook-category-tab-name")
-        for i, sub_clickable in enumerate(sub_clickables):
-            if i == 13:
+        for j, sub_clickable in enumerate(sub_clickables):
+            if j == 13:
                 break
-            if i == 1:
+            if j == 1:
                 print(1)
                 continue
             print(sub_clickable)
@@ -28,6 +30,7 @@ def test_clicking(driver, db):
             dk_extract_data(sub_url, driver, db)
         url = driver.current_url
         print(url)
+        i += 1
 
 def dk_extract_data(url, driver, db):
     driver.get(url)
@@ -45,13 +48,12 @@ def dk_extract_data(url, driver, db):
             game_day_data[date_text] = div
 
     parsed_data = wp.parse_data(game_day_data, cts.DraftKingsConstants.TEAM_TYPE, cts.DraftKingsConstants.TEAM_HTML,
-                            cts.DraftKingsConstants.ML_TYPE, cts.DraftKingsConstants.ML_HTML)
+                                cts.DraftKingsConstants.ML_TYPE, cts.DraftKingsConstants.ML_HTML)
     wp.write_data(parsed_data, "football", "dk", db)
 
 def clicking_b365(driver):
     action = ActionChains(driver)
     driver.get("https://www.co.bet365.com/?_h=7zGwoPT5idsD__-vUHcGpw%3D%3D&btsffd=1#/HO/")
-    
 
 if __name__ == '__main__':
     unittest.main()
